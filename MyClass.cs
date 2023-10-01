b137c539e3cef9be7f429fca25e30a90930e32cf
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-01 09:00:50 -0800

    async Await whit Task

---
+++ MyClass.cs
@@ -0,0 +1,15 @@
+﻿namespace _006_AsyncAwait
+{
+    internal class MyClass
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Primary Thread Id = {0}", Environment.CurrentManagedThreadId);
+        }
+
+        public async Task OperationAsync()
+        {
+            await Task.Factory.StartNew(Operation);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using _006_AsyncAwait;
+
+MyClass my = new ();
+
+Task task = my.OperationAsync();
+
+await task.ContinueWith(t => Console.WriteLine("Continuing the task"));
+
+Console.ReadLine();
